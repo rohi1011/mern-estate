e27d78b57faca25c27b5438a5705942005c845a7
Rohith <rrohith1011@gmail.com>
2023-12-26 17:27:15 +0530

    add redux persist

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then ( () => {
- console.log('Connected to MongoDB!');
 }).catch((err) => {
 console.log(err);
 });
